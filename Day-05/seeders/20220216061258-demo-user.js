11375a4b2980a807eb289d67def51bf19f53ee2f
Menahal Tanveer <menahaltanveer@Administrators-MacBook-Pro.local>
2022-02-16 12:45:02 +0500

    bulk insert using seeds

---
+++ Day-05/seeders/20220216061258-demo-user.js
@@ -0,0 +1,32 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Users", [
+      {
+        firstName: "John",
+        lastName: "Doe",
+        email: "example@example.com",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        firstName: "Ella",
+        lastName: "Doe",
+        email: "example@example.com",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete("Users", null, {});
+  },
+};
