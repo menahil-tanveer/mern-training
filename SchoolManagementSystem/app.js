a46026b0af9f1beb5475230e52d8e85694c7013f
Menahil <99386372+menahil-tanveer@users.noreply.github.com>
2022-03-02 23:04:29 +0500

    swagger init

--- SchoolManagementSystem/app.js
+++ SchoolManagementSystem/app.js
@@ -1,5 +1,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
 const userRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes");
@@ -19,6 +21,26 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/admins", adminRoutes);
 app.use("/api/courses", courseRoutes);
+// SWAGGER DOCS
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "School Management System",
+      version: "1.0.0",
+      description: "School ",
+    },
+    servers: [
+      {
+        url: "http://localhost:8082",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 app.listen(port, () => {
   console.log("Server is running on::::", port);
 });
--- SchoolManagementSystem/routes/adminRoutes.js
+++ SchoolManagementSystem/routes/adminRoutes.js
@@ -10,6 +10,63 @@ const router = express.Router();
 const adminMiddleware = require("../middlewares/adminMiddleware");
 const adminController = require("../controllers/adminController");
 const authenticateToken = require("../middlewares/authenticateToken");
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Admin:
+ *       type: object
+ *       required:
+ *         - adminId
+ *         - fullName
+ *         - email
+ *         - password
+ *       properties:
+ *          id:
+ *           type: string
+ *           description: unique id for admin
+ *          fullName:
+ *           type: string
+ *           description: Full name of admin
+ *          email:
+ *           type: string
+ *           description: Valid email address
+ *          password:
+ *           type: string
+ *           description: 8 character password with at least one digit
+ *       example:
+ *         adminId: ADMIN-MIKE-001
+ *         fullName:  Mike Kelly
+ *         email: mike.kelly@gmail.com
+ *         password: 1234567ABC
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Admin
+ *   description: Admin managing API
+ */
+
+// Routes
+/**
+ * @swagger
+ * /api/admin/:
+ *   get:
+ *     summary: Returns the list of all admins
+ *     tags: [Admins]
+ *     responses:
+ *       200:
+ *         description: The list of the admins
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Admin'
+ */
+
 router.get("/", [authenticateToken.verifyToken], adminController.getAllAdmins);
 router.get(
   "/get-admin/:adminId",
