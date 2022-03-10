67d76b249e4f468c5c3a4f1175138d828fb020fb
Menahil <99386372+menahil-tanveer@users.noreply.github.com>
2022-03-10 10:04:28 +0500

    testing lifecycle hooks

--- SchoolManagementSystem-frontend/frontend/src/components/Layout.jsx
+++ SchoolManagementSystem-frontend/frontend/src/components/Layout.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
-
+// NOTE: Lifecycle hooks can only be used in class components not stateless components
 class BaseLayout extends Component {
   state = {};
+  constructor(props) {
+    super(props);
+    console.log("BaseLayout - constructor");
+    // undefined props obj
+    console.log("props BaseLayout", props);
+  }
+  // perfect place for making ajax calls
+  componentDidMount() {
+    // i.e. component is in the DOM
+    console.log("BaseLayout -  mounted");
+  }
+  componentDidUpdate() {
+    // when props or state is updated
+    console.log("BaseLayout -  updated");
+  }
   render() {
+    console.log("BaseLayout rendered");
     return <React.Fragment></React.Fragment>;
   }
 }
